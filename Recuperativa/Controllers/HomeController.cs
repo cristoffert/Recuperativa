85f1e064c3e73e84b86922735a1da5c3c5cda425
cristoffert <cristoffertorrealba@gmail.com>
2017-12-27 19:42:38 -0300

    recuperativa finalizada

---
+++ Recuperativa/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Recuperativa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalesVaca.Controllers
+{
+    public class HomeController : Controller
+    {
+        RECUPERATIVAEntities cnx;
+        public HomeController()
+        {
+            cnx = new RECUPERATIVAEntities();
+        }
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        public ActionResult Guardar(int diio, string nombre, string sexo, string raza, string leche)
+        {
+
+            Recuperativa.Models.Vaca vacas = new Recuperativa.Models.Vaca()
+            {
+                Diio = diio,
+                Nombre = nombre,
+                Sexo = sexo,
+                Raza = raza,
+                Leche = leche
+            };
+
+            cnx.Vaca.Add(vacas);
+            cnx.SaveChanges();
+
+            return View("Listado", ListadoVacas());
+        }
+        public ActionResult Listado()
+        {
+
+            return View("Listado", ListadoVacas());
+        }
+        public ActionResult Ficha(int diio)
+        {
+            Vaca vaca = (Vaca)cnx.Vaca.Where(x => x.Diio==diio).First();
+
+            return View("Ficha", vaca);
+        }
+        private List<Recuperativa.Models.Vaca> ListadoVacas()
+        {
+
+            cnx.Database.Connection.Open();
+            List<Recuperativa.Models.Vaca> vacas = cnx.Vaca.ToList();
+
+            cnx.Database.Connection.Close();
+
+            return vacas;
+        }
+    }
+}
---
+++ Recuperativa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recuperativa.MvcApplication" Language="C#" %>
